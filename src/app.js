3ba9671595741f0f352ec110dc3bdcca7951a4da
julialozmin <julialozmin@gmail.com>
2024-02-13 19:57:00 +0100

    Added function runClocks to clear the intervall
    Removing alerts from console due to setInterval running in the background

--- src/app.js
+++ src/app.js
@@ -1,30 +1,34 @@
+let timer;
+
 function updateTime(cityId, timezone) {
   let cityElement = document.querySelector(cityId);
-  let cityDateElement = cityElement.querySelector(".date");
-  let cityTimeElement = cityElement.querySelector(".time");
+  const cityDateElement = cityElement.querySelector(".date");
+  const cityTimeElement = cityElement.querySelector(".time");
   let cityTime = moment().tz(timezone);
   cityDateElement.innerHTML = cityTime.format("MMMM Do YYYY");
   cityTimeElement.innerHTML = cityTime.format("hh:mm:ss [<small>]A[</small>]");
 }
 
-function updateAll() {
-  updateTime("#los-angeles", "America/Los_Angeles");
-  updateTime("#sydney", "Australia/Sydney");
-  updateTime("#tokyo", "Asia/Tokyo");
+function runClocks() {
+  function updateAll() {
+    updateTime("#los-angeles", "America/Los_Angeles");
+    updateTime("#sydney", "Australia/Sydney");
+    updateTime("#tokyo", "Asia/Tokyo");
+  }
+  timer = setInterval(updateAll, 1000);
 }
 
-setInterval(updateAll, 1000);
-
 function showSelectedCity(event) {
+  clearInterval(timer);
   function updateSelected() {
     let cityTimezone = event.target.value;
     if (cityTimezone === "current") {
       cityTimezone = moment.tz.guess();
     }
-    let cityName = cityTimezone.replace("_", " ").split(`/`)[1];
-    let cityDisplay = document.querySelector("#cities");
-    let selectedCityDate = moment().tz(cityTimezone).format("MMMM Do YYYY");
-    let selectedCityTime = moment()
+    const cityName = cityTimezone.replace("_", " ").split(`/`)[1];
+    const cityDisplay = document.querySelector("#cities");
+    const selectedCityDate = moment().tz(cityTimezone).format("MMMM Do YYYY");
+    const selectedCityTime = moment()
       .tz(cityTimezone)
       .format("hh:mm:ss [<small>]A[</small>]");
 
@@ -43,3 +47,5 @@ function showSelectedCity(event) {
 let citiesSelect = document.querySelector("#cities-dropdown");
 
 citiesSelect.addEventListener("change", showSelectedCity);
+
+runClocks();
